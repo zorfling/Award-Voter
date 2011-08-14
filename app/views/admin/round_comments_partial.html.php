fcc443ae31da9ea9b906e4c94d9238c52b59ac52
Chris Colborne <zorfling@gmail.com>
2011-08-14 18:59:55 +1000

    Don't write p tag for null comments

--- app/views/admin/round_comments_partial.html.php
+++ app/views/admin/round_comments_partial.html.php
@@ -1,9 +1,15 @@
 <h4>Comments for Round <?= $roundId; ?> for <?= $user->getFullName(); ?></h4>
-<?php foreach ($votes as $vote) { ?>
+<?php 
+foreach ($votes as $vote) { 
+	if (trim($vote->comments) != '') {
+	?>
 
 <p><?= $vote->comments; ?></p>
 
-<?php } ?>
+<?php 
+	} 
+}
+?>
 
 
 <?= $this->html->link('Back to results', '/admin/round/results/'.$roundId.'/'); ?>
