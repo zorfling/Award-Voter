06a37712078b539aef01d57dac072788eafd4f3d
Chris Colborne <zorfling@gmail.com>
2011-09-16 10:11:52 +1000

    Fix always hacked error

--- app/controllers/AwardsController.php
+++ app/controllers/AwardsController.php
@@ -78,7 +78,7 @@ class AwardsController extends \lithium\action\Controller {
 						}
 						
 						// If voter or votee is not in the round, someone is playing with the post
-						if (!(in_array($voterId, $userIds) && in_array($voteeId, $userIds))) {
+						if (!in_array($voteeId, $userIds)) {
 							FlashMessage::write("Naughty naughty! Please don't hack me!");
 							break;
 						}
